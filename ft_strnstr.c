4b8a3bf22ff56d225636275fb69d91c7631c3e54
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-02 17:38:56 +0200

    final2nd Oct

---
+++ ft_strnstr.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/02 16:53:29 by fsitter           #+#    #+#             */
+/*   Updated: 2025/10/02 16:59:39 by fsitter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strnstr(const char *big, const char *little, size_t len);
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+	
+}
+int	main(void)
+{
+	
+}
