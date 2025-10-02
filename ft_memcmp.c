2c1388b5ff3cb5fa0f93aa3baa393594acf466fd
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-02 09:34:34 +0200

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,28 +6,41 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 17:40:23 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/01 17:44:35 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/02 09:34:00 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_memcmp(const void *s1, const void *s2, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
-int ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	return (42);
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = s1;
+	str2 = s2;
+	while (n > 0)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+		n--;
+	}
+	return (0);
 }
 
-int	main (void)
-{
-	char *s1 = "hello";
-	char *s2 = "hello";
-	int len = 3;
+// int	main(void)
+// {
+// 	char *s1 = "hello";
+// 	char *s2 = "xello";
+// 	int len = 3;
 
-	int ft = ft_memcmp(s1, s2, len);
-	int og = memcmp(s1, s2, len);
+// 	int ft = ft_memcmp(s1, s2, len);
+// 	int og = memcmp(s1, s2, len);
 
-	printf("%i\n", ft);
-	printf("%i\n", og);
-}
+// 	printf("FT: %i\n", ft);
+// 	printf("OG: %i\n", og);
+// }
