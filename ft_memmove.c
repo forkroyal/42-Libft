e88167c856e3d1b1d5c2fd79ca2642f18e2ca1f5
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-02 11:53:50 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 11:48:46 by fsitter           #+#    #+#             */
-/*   Updated: 2025/09/30 11:55:53 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/02 11:53:29 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,45 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	char		*mydest;
+	const char	*mysrc;
+	int			i;
+
+	if (!dest || !src)
+		return (0);
+	mydest = dest;
+	mysrc = src;
+	i = 0;
+	if (dest < src)
+	{
+		while (i++ < n)
+			mydest[i] = mysrc[i];
+	}
+	else
+	{
+		while (i++ < n)
+			mydest[n - i - 1] = mysrc[n - i - 1];
+	}
+	return (dest);
 }
+
+// int	main(void)
+// {
+// 	int	src[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+// 	int	dest[10];
+// 	int	len;
+// 	int	plus_dest;
+// 	int	copy_this_much;
+// 	int	i;
+
+// 	len = sizeof(src) / sizeof(src[0]);
+// 	plus_dest = 2;
+// 	copy_this_much = len - plus_dest;
+// 	ft_memmove(src + plus_dest, src, sizeof(int) * copy_this_much);
+// 	i = 0;
+// 	while (i < 10)
+// 	{
+// 		printf("%i\n", src[i]);
+// 		i++;
+// 	}
+// }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:33:28 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/01 17:40:44 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/02 11:53:37 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-//void	*ft_memmove(void *dest, const void *src, size_t n); // memmove not done
+void	*ft_memmove(void *dest, const void *src, size_t n); // memmove not done
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 // strlcat
 int		ft_toupper(int c);
@@ -36,7 +36,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-// int ft_memcmp(const void *s1, const void *s2, size_t n);
+int 	ft_memcmp(const void *s1, const void *s2, size_t n);
 // strnstr
 int		ft_atoi(const char *nptr);
 
