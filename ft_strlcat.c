9c982a5c88cff649210ce7dc4e691a202a2e183a
forkroyal <gfabits12@gmail.com>
2025-10-03 17:06:20 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 14:51:12 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/02 16:53:16 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/03 17:05:48 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 		return (size + src_len);
 	else
 	{
-		while (src[i] && i < size - 1)
+		while (src[i] && dst_len + i < size - 1)
 		{
 			dst[dst_len + i] = src[i];
 			i++;
@@ -51,8 +51,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 // int	main(void)
 // {
 // 	char	src1[6] = "Hello";
-// 	char	dest1[4] = "Bye";
+// 	char	dest1[20] = "Bye";
 
-// 	printf("%zu\n", ft_strlcat(dest1, src1, 2));
+// 	printf("%zu\n", ft_strlcat(dest1, src1, 9));
 // 	printf("%s\n", dest1);
 // }
