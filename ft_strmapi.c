74f48466ce8890b50119be3cf35bad02cd427e03
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-08 12:51:28 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/08 12:41:26 by fsitter           #+#    #+#             */
+/*   Updated: 2025/10/08 12:51:06 by fsitter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	int	len;
+	char	*mapped;
+
+	i = 0;
+	len = ft_strlen(s);
+	mapped = malloc(sizeof(char) * len + 1);
+	if (!mapped)
+		return (NULL);
+	while (i < len)
+	{
+		mapped[i] = f(i, s[i]);
+		i++;
+	}
+	mapped[i] = '\0';
+	return (mapped);
+}
+
+// char		fs_toupper(unsigned int a, char c);
+
+// char		fs_toupper(unsigned int a, char c)
+// {
+// 	(void)a;
+// 	if (c >= 'a' && c <= 'z')
+// 		return (c - 32);
+// 	else
+// 		return (c);
+// }
+
+// int	main(void)
+// {
+// 	char hallo[] = "hallo";
+// 	char *s;
+
+// 	s = ft_strmapi(hallo, fs_toupper);
+// 	printf("%s\n", s);
+// }
