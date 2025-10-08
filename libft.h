0041722b2cca63138da155a4533ba254e5fa4cd5
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-10-08 13:32:08 +0200

    striteri

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 13:33:28 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/08 12:02:11 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/08 13:31:58 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,8 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 // char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
-// ft_strmapi
-// ft_striteri
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
