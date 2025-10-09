c131f49b1ba8a93e8a4ba579d8419b8e60d7e276
forkroyal <gfabits12@gmail.com>
2025-10-09 12:29:59 +0200

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:16:06 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/01 15:13:50 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/09 12:29:48 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n - 1)
+	while (s1[i] && s2[i] && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (0);
 }
 // int	main(void)
 // {
-// 	char *one = "abcd";
-// 	char *two = "abc";
-// 	int len = 4;
+// 	char *one = "t";
+// 	char *two = "";
+// 	int len = 0;
 // 	printf("FT: %i\n", ft_strncmp(one, two, len));
 // 	printf("OR: %i\n", strncmp(one, two, len));
 // }
