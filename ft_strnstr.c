0f5aa3931b37538a1dee6236d247286f763ce351
forkroyal <gfabits12@gmail.com>
2025-10-09 12:36:24 +0200

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 16:53:29 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/07 10:35:19 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/09 12:36:05 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,23 @@ char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t i, j;
+	size_t i;
+	size_t j;
 	i = 0;
 	if (little[0] == '\0')
 		return ((char *)big);
 	while (big[i] && i < len)
 	{
 		j = 0;
-		while (big[i+j] != '\0' && big[i+j] == little[j] && j < len)
+		while (big[i + j] != '\0' && big[i + j] == little[j] && j < len)
 		{
-			if (little[j+1] == '\0')
+			if (little[j + 1] == '\0')
 				return ((char *)&big[i]);
 			j++;
 		}
 		i++;
 	}
-	return(0);
+	return (0);
 }
 
 // int	main(void)
