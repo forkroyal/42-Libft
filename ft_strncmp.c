6969d5c7eb3e2623d352f606259cbf044f4487c9
forkroyal <gfabits12@gmail.com>
2025-10-10 15:18:37 +0200

    '600'

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:16:06 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/10 15:08:25 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 15:14:09 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while ((str1[i] || str2[i]) && i < n - 1)
+	while ((str1[i] || str2[i]) && i < n )
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
@@ -36,9 +36,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 // int	main(void)
 // {
-// 	char *one = "lorem ipsum dolor sit amet";
-// 	char *two = "dolor";
-// 	int len = 15;
+// 	char *one = "1";
+// 	char *two = "";
+// 	int len = 0;
 // 	printf("FT: %i\n", ft_strncmp(one, two, len));
 // 	printf("OR: %i\n", strncmp(one, two, len));
 // }
