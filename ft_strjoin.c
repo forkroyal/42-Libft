ba59a865ec9a883bdad2522f0ccf708664770e80
forkroyal <gfabits12@gmail.com>
2025-10-10 11:35:00 +0200

    '600'

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 13:28:38 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/06 13:41:34 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:34:57 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
+	if (!s1 || !s2)
+		return (NULL);
 	len_joined = ft_strlen(s1) + ft_strlen(s2) + 1;
 	joined = malloc(sizeof(char) * len_joined);
 	if (!joined)
@@ -28,11 +30,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	while (s1[i])
-		joined[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		joined[j++] = s2[i++];
-	joined[j] = '\0';
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
 	return (joined);
 }
 
