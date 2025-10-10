69e0e79dd5f239851e6e20d2bd0fbff03c2f4b27
forkroyal <gfabits12@gmail.com>
2025-10-10 11:59:20 +0200

    '600'

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:16:06 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/09 12:29:48 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:59:09 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
 	while (s1[i] && s2[i] && i < n)
 	{
