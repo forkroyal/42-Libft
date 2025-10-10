d06b74388573ab9bb715e3a59774f7e6e4bb262d
forkroyal <gfabits12@gmail.com>
2025-10-10 16:04:22 +0200

    '600'

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 10:20:58 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/10 14:40:40 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 16:02:35 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,16 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	size_t	mione;
 
 	mione = -1;
-	if (nmemb == 0 || size == 0)
-		return (ft_strdup(""));
+	if (nmemb <= 0 || size <= 0)
+	{
+		pointer = malloc(0);
+		if (!pointer)
+			return (NULL);
+		return (pointer);
+	}
 	if (mione / size < nmemb)
 		return (NULL);
-	bytesize = nmemb + size;
+	bytesize = nmemb * size;
 	pointer = malloc(bytesize);
 	if (!pointer)
 		return (NULL);
@@ -36,13 +41,18 @@ void	*ft_calloc(size_t nmemb, size_t size)
 // int	main(void)
 // {
 // 	char *pointer;
+// 	char *pointer2;
 
-// 	// pointer = ft_calloc(NULL, sizeof(char));
-// 	// printf("%p\n", pointer);
+// 	size_t var1 = 0;
 
-// 	pointer = calloc(NULL, sizeof(char));
+// 	pointer = ft_calloc(var1, sizeof(char));
 // 	printf("%p\n", pointer);
+// 	free(pointer);
+// 	pointer = NULL;
 
-	
+// 	pointer2 = calloc(var1, sizeof(char));
+// 	printf("%p\n", pointer2);
+// 	free(pointer2);
+// 	pointer2 = NULL;
 
 // }
