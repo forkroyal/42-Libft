758b68fff7673cc156dbc34fede2adca37f5e382
forkroyal <gfabits12@gmail.com>
2025-10-10 11:46:24 +0200

    '600'

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 12:01:08 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/09 12:21:22 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 11:46:21 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (size <= 0)
+		return (ft_strlen(src));
 	while (src[i] != '\0' && (size - 1) > i)
 	{
 		dst[i] = src[i];
@@ -29,13 +31,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (ft_strlen(src));
 }
 
-int	main(void)
-{
-	char dest[6] = {"hello"};
-	char src[5] = {"byby"};
-	printf("%s\n", dest);
-	ft_strlcpy(dest, src, 0);
-	// strlcpy(dest, src, 9);
-	printf("%s\n", dest);
-	printf("%li\n", ft_strlcpy(dest, src, 0));
-}
+// int	main(void)
+// {
+// 	char dest[6] = {"hello"};
+// 	char src[5] = {"byby"};
+// 	printf("%s\n", dest);
+// 	ft_strlcpy(dest, src, 0);
+// 	// strlcpy(dest, src, 9);
+// 	printf("%s\n", dest);
+// 	printf("%li\n", ft_strlcpy(dest, src, 0));
+// }
