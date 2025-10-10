67e4e5dd65505b6830a64af6df7bbffa7acdda1f
forkroyal <gfabits12@gmail.com>
2025-10-10 15:09:08 +0200

    '600'

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 14:16:06 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/10 11:59:09 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 15:08:25 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,29 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	if (n == 0)
 		return (0);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((str1[i] || str2[i]) && i < n - 1)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);
 }
+
 // int	main(void)
 // {
-// 	char *one = "t";
-// 	char *two = "";
-// 	int len = 0;
+// 	char *one = "lorem ipsum dolor sit amet";
+// 	char *two = "dolor";
+// 	int len = 15;
 // 	printf("FT: %i\n", ft_strncmp(one, two, len));
 // 	printf("OR: %i\n", strncmp(one, two, len));
 // }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 12:24:16 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/10 14:37:59 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/10 15:09:04 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	if (!s)
 		return (NULL);
 	len_of_s = ft_strlen(s);
-	if (start > len)
+	if (start > len_of_s)
 		return (NULL);
 	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
