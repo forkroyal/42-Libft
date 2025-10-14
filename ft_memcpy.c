f9d0479ab73185ced9f5bbbdc96d591d9617f2d0
forkroyal <gfabits12@gmail.com>
2025-10-14 09:11:52 +0200

    '600'

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: fsitter <fsitter@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:41:01 by fsitter           #+#    #+#             */
-/*   Updated: 2025/10/13 14:03:19 by fsitter          ###   ########.fr       */
+/*   Updated: 2025/10/14 09:10:17 by fsitter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char	*mysrc;
 	size_t			i;
 
+	while (dest == NULL && src == NULL && n)
+		return (NULL);
 	i = 0;
 	mydest = (unsigned char *)dest;
 	mysrc = (unsigned char *)src;
@@ -33,18 +35,17 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 // #include <stdio.h>
 // #include <string.h>
+
 // int	main(void)
 // {
-
 // 	printf("hello\n");
-// 	//ft_memcpy(NULL, NULL, 2); //segv segv
-// 	//ft_memcpy(NULL, NULL, 0); //safe safe
-// 	//ft_memcpy("hello", NULL, NULL); //safe safe
-// 	//ft_memcpy("hello", NULL, 1); //segv segv
-// 	//memcpy(NULL, "hello", 0); //safe safe
-// 	//ft_memcpy(NULL, "hello", 2); //sagv segv
+// 	ft_memcpy(NULL, NULL, 3); // segv segv
+// 	// ft_memcpy(NULL, NULL, 0); //safe safe
+// 	// ft_memcpy("hello", NULL, NULL); //safe safe
+// 	// ft_memcpy("hello", NULL, 1); //segv segv
+// 	// memcpy(NULL, "hello", 0); //safe safe
+// 	// ft_memcpy(NULL, "hello", 2); //sagv segv
 // 	printf("safe\n");
-
 // }
 
 // delete handeling
